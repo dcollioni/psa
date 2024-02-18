a37e9b4af9a5ead60a14cbd615a429de22d2cda0
Douglas Collioni <dcollioni@gmail.com>
2024-02-18 12:27:39 +0000

    add patient list page and fetch from api

--- UI/app/src/components/home/Home.tsx
+++ UI/app/src/components/home/Home.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className="home">
       <h2>Home - Welcome</h2>
-      <nav>
-        <Link to={'/patients'}>Go to patients</Link>
-      </nav>
     </div>
   )
 }
